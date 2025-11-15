6a6ded1f4fcecc0de9a365dd293caac26d432174
Huynh Duc Dung <dunghd.it@gmail.com>
2025-11-15 12:39:16 +0800

    fix(launcher): search for Love2D binary in system paths instead of local directory

--- mux_launch.sh
+++ mux_launch.sh
@@ -26,12 +26,27 @@ export LD_LIBRARY_PATH="$LOVEDIR/libs:$LD_LIBRARY_PATH"
 LOGFILE="${LOGDIR}/muos-collection-launch.log"
 echo "[DEBUG] Running as user: $(whoami)" >>"$LOGFILE"
 
+# Find Love2D binary (check multiple locations)
+LOVE_BIN=""
+if [ -f "/usr/bin/love" ]; then
+    LOVE_BIN="/usr/bin/love"
+elif [ -f "/usr/local/bin/love" ]; then
+    LOVE_BIN="/usr/local/bin/love"
+elif [ -f "$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/love2d/love" ]; then
+    LOVE_BIN="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/love2d/love"
+else
+    echo "[ERROR] Love2D binary not found" >>"$LOGFILE"
+    exit 1
+fi
+
+echo "[DEBUG] Using Love2D: $LOVE_BIN" >>"$LOGFILE"
+
 # Start gptokeyb (gamepad to keyboard mapper)
 $GPTOKEYB "love" &
 GPTOKEYB_PID=$!
 
 # Launch app and capture output
-./love . --project-root="$LOVEDIR" >>"$LOGFILE" 2>&1
+"$LOVE_BIN" "$LOVEDIR" >>"$LOGFILE" 2>&1
 
 # Kill gptokeyb
 kill -9 "$GPTOKEYB_PID"
